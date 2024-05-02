0ef6f726c1c2ab4d1bba24f27fb5b0f32363cbe6
maomao0007 <skylifepic07@gmail.com>
2024-05-02 21:59:22 +0800

    feat: Implemented functionality to generate random password in app.js

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
 const express =  require('express')
+const { engine } = require("express-handlebars");
 const app = express()
 const port = 3000
+const generatePassword = require("./utils/generatePW");
 
-app.get('/', (req, res) => {
-  res.send ('express app')
-})
+app.engine(".hbs", engine({ extname: ".hbs" }));
+app.set("view engine", ".hbs");
+app.set("views", "./views");
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.render("index", { isResultVisible: false }); // 將 isResultVisible 設置為 false
+});
+
+app.post("/generate", (req, res) => {
+  const options = req.body;
+  const password = generatePassword(options);
+
+  res.render("index", { password, isResultVisible: true }); // 將生成的密碼和 isResultVisible 設置為 true 傳遞給模板
+});
 
 app.listen(port, () => {
  console.log(`express server is running on http://localhost:${port}`)
